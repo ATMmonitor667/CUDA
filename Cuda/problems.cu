fba429b57648b253e918d27cf46e8bbf9e227418
A.T.M. Hossain <rhossainwork0645@gmail.com>
2025-09-08 15:13:04 -0400

    initialized commit

---
+++ Cuda/problems.cu
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cuda_runtime.h>
+
+
+
+
+
+
+
+int main(void)
+{
+    //int count;
+    //cudaGetDeviceCount(&count);
+    //std::cout << "Number of CUDA devices: " << count << std::endl;
+    return 0;
+}
